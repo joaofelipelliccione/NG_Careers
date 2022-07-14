7ba273ad09c98bc5c88b33b0a8bdf6229671d2ec
João Felipe P <jf.pelliccione@gmail.com>
2022-07-14 17:58:27 -0300

    Conclusão Cover!

--- src/components/Cover.jsx
+++ src/components/Cover.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import coverMobile from '../assets/cover_mobile.png';
+import coverDesktop from '../assets/cover_desktop.png';
 import '../styles/cover.css';
 
 function Cover() {
+  const isMobileDevice = useMediaQuery({ query: '(max-width: 767px)' });
+
   return (
     <section className="coverSection">
-      <img src={ coverMobile } alt="Logo NG.CASH" />
+      {isMobileDevice ? (
+        <img src={ coverMobile } alt="Logo NG.CASH" className="mobileCover" />
+      ) : (
+        <img src={ coverDesktop } alt="Logo NG.CASH" className="desktopCover" />
+      )}
     </section>
   );
 }
