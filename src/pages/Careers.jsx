06acbde8b6c315dc68563975c4b41fda62b69fd9
João Felipe P <jf.pelliccione@gmail.com>
2022-07-15 00:14:45 -0300

    Continuando...

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -11,11 +11,24 @@ import trindadePicMob from '../assets/statements/mobile/trindade_pic_mob.png';
 import trindadeTextMob from '../assets/statements/mobile/trindade_text_mob.png';
 import trindadePicDesktop from '../assets/statements/desktop/trindade_pic_desk.png';
 import trindadeTextDesktop from '../assets/statements/desktop/trindade_text_desk.png';
-// import paulaPicMob from '../assets/statements/mobile/paula_pic_mob.png';
-// import paulaTextMob from '../assets/statements/mobile/paula_text_mob.png';
+import paulaPicMob from '../assets/statements/mobile/paula_pic_mob.png';
+import paulaTextMob from '../assets/statements/mobile/paula_text_mob.png';
 // import paulaPicDesktop from '../assets/statements/desktop/paula_pic_desk.png';
 // import paulaTextDesktop from '../assets/statements/desktop/paula_text_desk.png';
 import '../styles/careers.css';
+
+const statementsInfoMob = [
+  { name: 'Victor Trindade',
+    bgColor: '#7D2CFF',
+    personPic: trindadePicMob,
+    personStatPic: trindadeTextMob,
+  },
+  { name: 'Paula Cruz',
+    bgColor: '#FFFFFF',
+    personPic: paulaPicMob,
+    personStatPic: paulaTextMob,
+  },
+];
 
 function Careers() {
   const isMobileDevice = useMediaQuery({ query: '(max-width: 767px)' });
@@ -28,11 +41,14 @@ function Careers() {
       <NewsSection />
       <SecondCover />
       {isMobileDevice ? (
-        <MobileStatement
-          bkgrndColor="#7D2CFF"
-          personPicture={ trindadePicMob }
-          personStatementPicture={ trindadeTextMob }
-        />)
+        statementsInfoMob.map(({ name, bgColor, personPic, personStatPic }) => (
+          <MobileStatement
+            key={ name }
+            bkgrndColor={ bgColor }
+            personPicture={ personPic }
+            personStatementPicture={ personStatPic }
+          />
+        )))
         : (
           <DesktopStatement
             bkgrndColor="#7D2CFF"
