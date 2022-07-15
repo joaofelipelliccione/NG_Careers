30b1ddee7b8c517cd8b2184bf553cb9a28f2c039
João Felipe P <jf.pelliccione@gmail.com>
2022-07-15 17:43:42 -0300

    Continuando...

--- src/components/NewsSection.jsx
+++ src/components/NewsSection.jsx
@@ -10,12 +10,12 @@ import newsBillboard from '../assets/news_billboard.png';
 import '../styles/newsSection.css';
 
 const newsIcons = [
-  { iconId: 1, iconImgSrc: logoGlobo, iconHref: 'https://oglobo.globo.com/' },
-  { iconId: 2, iconImgSrc: logoForbes, iconHref: 'https://forbes.com.br/' },
-  { iconId: 3, iconImgSrc: logoEstadao, iconHref: 'https://www.estadao.com.br/' },
-  { iconId: 4, iconImgSrc: logoFolhaSP, iconHref: 'https://www.folha.uol.com.br/' },
-  { iconId: 5, iconImgSrc: logoIstoE, iconHref: 'https://istoe.com.br/' },
-  { iconId: 6, iconImgSrc: logoUol, iconHref: 'https://www.uol.com.br/' },
+  { iconId: 1, iconImgSrc: logoGlobo, iconHref: 'https://oglobo.globo.com/clube-o-globo/servicos/oferta/ngcash-assine-e-ganhe.ghtml' },
+  { iconId: 2, iconImgSrc: logoForbes, iconHref: 'https://forbes.com.br/forbes-money/2021/06/segmentacao-de-fintechs-conheca-4-bancos-digitais-criados-para-publicos-especificos/#foto4' },
+  { iconId: 3, iconImgSrc: logoEstadao, iconHref: 'https://einvestidor.estadao.com.br/mercado/banco-neaglebank-youtubers-criancas/' },
+  { iconId: 4, iconImgSrc: logoFolhaSP, iconHref: 'https://www1.folha.uol.com.br/mpme/2021/05/empresas-lancam-aplicativos-de-mesada-digital-e-cartao-de-credito-para-criancas.shtml' },
+  { iconId: 5, iconImgSrc: logoIstoE, iconHref: 'https://www.istoedinheiro.com.br/tem-fintech-ate-para-criancas-e-adolescentes/' },
+  { iconId: 6, iconImgSrc: logoUol, iconHref: 'https://economia.uol.com.br/mais/ultimas-noticias/2021/01/27/fintechs-bancos-digitais-para-criancas-adolescentes.htm' },
 ];
 
 function NewsSection() {
