e4223a346408434f1f9475c604b865a6ac2eb8b0
João Felipe P <jf.pelliccione@gmail.com>
2022-07-14 22:29:44 -0300

    Continuando...

--- src/components/JoinNgBtn.jsx
+++ src/components/JoinNgBtn.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 import '../styles/joinNgBtn.css';
 
 function JoinNgBtn() {
+  const isMobileDevice = useMediaQuery({ query: '(max-width: 767px)' });
+
   return (
     <a
-      className="joinNgBtn"
+      className={ isMobileDevice ? 'joinNgBtnMobile' : 'joinNgBtnDesktop' }
       href="https://boards.greenhouse.io/ngcash?t=f684d1ae5us"
       target="_blank"
       rel="noreferrer"
