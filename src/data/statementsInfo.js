0ba3d46dba42f86b1e5c307517285df0fc1c6954
João Felipe P <jf.pelliccione@gmail.com>
2022-07-15 17:29:14 -0300

    Continuando...

--- src/data/statementsInfo.js
+++ src/data/statementsInfo.js
@@ -1,6 +1,6 @@
 import trindadePicMob from '../assets/statements/mobile/trindade_pic_mob.png';
 import trindadeTextMob from '../assets/statements/mobile/trindade_text_mob.svg';
-import trindadePicDesktop from '../assets/statements/desktop/trindade_pic_desk.svg';
+import trindadePicDesktop from '../assets/statements/desktop/trindade_pic_desk.png';
 import trindadeTextDesktop from '../assets/statements/desktop/trindade_text_desk.svg';
 
 import paulaPicMob from '../assets/statements/mobile/paula_pic_mob.png';
@@ -10,12 +10,18 @@ import paulaTextDesktop from '../assets/statements/desktop/paula_text_desk.svg';
 
 import nadelmanPicMob from '../assets/statements/mobile/nadelman_pic_mob.png';
 import nadelmanTextMob from '../assets/statements/mobile/nadelman_text_mob.svg';
+import nadelmanPicDesktop from '../assets/statements/desktop/nadelman_pic_desk.png';
+import nadelmanTextDesktop from '../assets/statements/desktop/nadelman_text_desk.svg';
 
 // import kazuePicMob from '../assets/statements/mobile/kazue_pic_mob.png';
 // import kazueTextMob from '../assets/statements/mobile/kazue_text_mob.svg';
+// import kazuePicDesktop from '../assets/statements/desktop/meirot_pic_desk.png';
+// import kazueTextDesktop from '../assets/statements/desktop/meirot_text_desk.svg';
 
 import meirotPicMob from '../assets/statements/mobile/meirot_pic_mob.png';
 import meirotTextMob from '../assets/statements/mobile/meirot_text_mob.svg';
+import meirotPicDesktop from '../assets/statements/desktop/meirot_pic_desk.png';
+import meirotTextDesktop from '../assets/statements/desktop/meirot_text_desk.svg';
 
 const statementsInfoMob = [
   {
@@ -51,17 +57,40 @@ const statementsInfoMob = [
 ];
 
 export const statementsInfoDesktop = [
-  { name: 'Victor Trindade',
+  {
+    name: 'Victor Trindade',
     bgColor: '#7D2CFF',
     picSide: 'left',
     personPic: trindadePicDesktop,
     personStatPic: trindadeTextDesktop,
   },
-  { name: 'Paula Cruz',
+  {
+    name: 'Paula Cruz',
     bgColor: '#FFFFFF',
     picSide: 'right',
     personPic: paulaPicDesktop,
     personStatPic: paulaTextDesktop,
+  },
+  {
+    name: 'Eduardo Nadelman',
+    bgColor: '#C2C2C2',
+    picSide: 'left',
+    personPic: nadelmanPicDesktop,
+    personStatPic: nadelmanTextDesktop,
+  },
+  // {
+  //   name: 'Kazue Kottwitz',
+  //   bgColor: '#01CA30',
+  //   picSide: 'right',
+  //   personPic: kazuePicDesktop,
+  //   personStatPic: kazueTextDesktop,
+  // },
+  {
+    name: 'Victor Meirot',
+    bgColor: '#000000',
+    picSide: 'left',
+    personPic: meirotPicDesktop,
+    personStatPic: meirotTextDesktop,
   },
 ];
 
