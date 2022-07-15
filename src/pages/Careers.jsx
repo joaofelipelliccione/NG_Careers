52f6889515cc98d49a6fcd01f1fc18b436c01abb
João Felipe P <jf.pelliccione@gmail.com>
2022-07-15 00:03:35 -0300

    Continuando...

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -6,12 +6,16 @@ import JoinNgBtn from '../components/JoinNgBtn';
 import NewsSection from '../components/NewsSection';
 import SecondCover from '../components/SecondCover';
 import MobileStatement from '../components/statements/MobileStatement';
+import DesktopStatement from '../components/statements/DesktopStatement';
 import trindadePicMob from '../assets/statements/mobile/trindade_pic_mob.png';
 import trindadeTextMob from '../assets/statements/mobile/trindade_text_mob.png';
 import trindadePicDesktop from '../assets/statements/desktop/trindade_pic_desk.png';
 import trindadeTextDesktop from '../assets/statements/desktop/trindade_text_desk.png';
+// import paulaPicMob from '../assets/statements/mobile/paula_pic_mob.png';
+// import paulaTextMob from '../assets/statements/mobile/paula_text_mob.png';
+// import paulaPicDesktop from '../assets/statements/desktop/paula_pic_desk.png';
+// import paulaTextDesktop from '../assets/statements/desktop/paula_text_desk.png';
 import '../styles/careers.css';
-import DesktopStatement from '../components/statements/DesktopStatement';
 
 function Careers() {
   const isMobileDevice = useMediaQuery({ query: '(max-width: 767px)' });
