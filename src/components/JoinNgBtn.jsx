ce3278d15b47f95c94384616ed2ce08e383d0f00
João Felipe P <jf.pelliccione@gmail.com>
2022-07-18 18:58:30 -0300

    Fazendo alterações para a Kazue!

--- src/components/JoinNgBtn.jsx
+++ src/components/JoinNgBtn.jsx
@@ -1,19 +1,44 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive';
+import arrowToLeft from '../assets/arrow_to_left.svg';
+import arrowToRight from '../assets/arrow_to_right.svg';
 import '../styles/joinNgBtn.css';
 
 function JoinNgBtn() {
   const isMobileDevice = useMediaQuery({ query: '(max-width: 767px)' });
 
   return (
-    <a
-      className={ isMobileDevice ? 'joinNgBtnMobile' : 'joinNgBtnDesktop' }
-      href="https://boards.greenhouse.io/ngcash?t=f684d1ae5us"
-      target="_blank"
-      rel="noreferrer"
-    >
-      quero fazer parte do time!
-    </a>
+    isMobileDevice ? (
+      <a
+        className="joinNgBtnMobile"
+        href="https://boards.greenhouse.io/ngcash?t=f684d1ae5us"
+        target="_blank"
+        rel="noreferrer"
+      >
+        quero fazer parte do time!
+      </a>
+    ) : (
+      <a
+        className="joinNgBtnDesktop"
+        href="https://boards.greenhouse.io/ngcash?t=f684d1ae5us"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="arrows"
+          src={ arrowToRight }
+          alt="Seta p/ Direita"
+        />
+        <span>
+          quero fazer parte do time!
+        </span>
+        <img
+          className="arrows"
+          src={ arrowToLeft }
+          alt="Seta p/ Esquerda"
+        />
+      </a>
+    )
   );
 }
 
